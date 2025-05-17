a70d98cc2035f95625b4dfcfa6f4c12e44b7d1d9
Chandan Chaudhary <166887599+chandan9648@users.noreply.github.com>
2025-05-17 21:47:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function sendMessage() { 
     const input = document.getElementById("chatInput");    
-    const chatBox = document.getElementById("chatBox");       
+    const chatBox = document.getElementById("chatBox");        
     if (input.value.trim() !== "") {
       const message = document.createElement("div"); 
       message.innerHTML = `<strong>You:</strong> ${input.value}`;
